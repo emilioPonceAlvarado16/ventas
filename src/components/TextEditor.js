2f47a3b218ecc83fcdb7e76d913a75052911bd2c
Isaias <isaiasponce-12@hotmail.es>
2023-08-30 21:47:29 -0500

    Update: estilos del editor de texto

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,8 +1,26 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const TextEditor = () => {
   const editorRef = useRef(null);
+  const lineNumberRef = useRef(null);
   const [lineNumbers, setLineNumbers] = useState([1]);
+  const [highlightedLine, setHighlightedLine] = useState(0);
+
+  useEffect(() => {
+    editorRef.current.addEventListener('keyup', (e) => {
+      const selection = window.getSelection();
+      const range = selection.getRangeAt(0);
+      const rect = range.getBoundingClientRect();
+      const editorRect = editorRef.current.getBoundingClientRect();
+      const lineHeight = editorRef.current.offsetHeight / lineNumbers.length;
+      const position = Math.floor((rect.top - editorRect.top) / lineHeight);
+      setHighlightedLine(position);
+    });
+
+    editorRef.current.addEventListener('scroll', (e) => {
+      lineNumberRef.current.scrollTop = e.target.scrollTop;
+    });
+  }, [lineNumbers]);
 
   const handleContentChange = () => {
     const currentContent = editorRef.current.innerHTML;
@@ -31,9 +49,9 @@ const TextEditor = () => {
 
   return (
     <div style={{ display: 'flex', background: '#2c2c2c', height: '100vh', width: '50vw', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', overflow: 'hidden' }}>
-      <div style={{ marginRight: '1px', lineHeight: '1.2em', background: '#1a1a1a', padding: '10px', color: '#b5b5b5', borderRight: '1px solid #aaa' }}>
-        {lineNumbers.map((line) => (
-          <div key={line} style={{ height: '1.2em', borderBottom: '1px solid #aaa' }}>
+      <div ref={lineNumberRef} style={{ width: '50px', lineHeight: '1.2em', background: '#1a1a1a', padding: '10px 5px', color: '#b5b5b5', borderRight: '1px solid #aaa', overflowY: 'hidden' }}>
+        {lineNumbers.map((line, index) => (
+          <div key={line} style={{ height: '1.2em', background: highlightedLine === index ? '#3a3a3a' : 'transparent' }}>
             {line}
           </div>
         ))}
@@ -46,14 +64,13 @@ const TextEditor = () => {
         style={{
           padding: '10px',
           overflowY: 'auto',
-          width: 'calc(50vw - 60px)', // Restamos el ancho del div de números y el padding
+          flex: 1,
           overflowWrap: 'break-word',
           lineHeight: '1.2em',
           whiteSpace: 'pre-wrap',
           color: '#e5e5e5',
           fontSize: '16px',
-          fontFamily: 'Arial, sans-serif',
-          borderBottom: '1px solid #aaa'
+          fontFamily: 'Arial, sans-serif'
         }}
       ></div>
     </div>
