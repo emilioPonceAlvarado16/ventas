bb2a588a96a92a4b4a90cdadcd480ee3faffae33
Isaias <isaiasponce-12@hotmail.es>
2023-08-30 20:22:40 -0500

    added new text editor

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,22 +1,37 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 const TextEditor = () => {
   const editorRef = useRef(null);
   const [lineNumbers, setLineNumbers] = useState([1]);
 
   const handleContentChange = () => {
-    const currentContent = editorRef.current.innerText;
+    const currentContent = editorRef.current.innerHTML;
     updateLineNumbers(currentContent);
   };
 
-  const updateLineNumbers = (text) => {
-    const lines = text.split('\n');
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const selection = window.getSelection();
+      const range = selection.getRangeAt(0);
+      const br = document.createElement('br');
+      range.insertNode(br);
+      range.setStartAfter(br);
+      range.setEndAfter(br);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      handleContentChange();
+    }
+  };
+
+  const updateLineNumbers = (htmlContent) => {
+    const lines = htmlContent.split('<br>');
     setLineNumbers(lines.map((_, index) => index + 1));
   };
 
   return (
     <div style={{ display: 'flex' }}>
-      <div style={{ marginRight: '10px' }}>
+      <div style={{ marginRight: '10px', lineHeight: '1.2em' }}>
         {lineNumbers.map((line) => (
           <div key={line} style={{ height: '1.2em' }}>
             {line}
@@ -27,6 +42,7 @@ const TextEditor = () => {
         ref={editorRef}
         contentEditable={true}
         onInput={handleContentChange}
+        onKeyDown={handleKeyDown}
         style={{
           border: '1px solid #ccc',
           minHeight: '200px',
@@ -34,6 +50,8 @@ const TextEditor = () => {
           borderRadius: '4px',
           width: '300px',
           overflowWrap: 'break-word',
+          lineHeight: '1.2em',
+          whiteSpace: 'pre-wrap'
         }}
       ></div>
     </div>
