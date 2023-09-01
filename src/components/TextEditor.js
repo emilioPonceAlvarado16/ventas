679cc68169f3692c40ee30383cebc33e3cd94345
Isaias <isaiasponce-12@hotmail.es>
2023-09-01 17:01:58 -0500

    hoy 01/09

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -6,17 +6,14 @@ const TextEditor = () => {
   const [lineNumbers, setLineNumbers] = useState([1]);
   const [highlightedLine, setHighlightedLine] = useState(0);
 
-  useEffect(() => {
-    editorRef.current.addEventListener('keyup', (e) => {
-      const selection = window.getSelection();
-      const range = selection.getRangeAt(0);
-      const rect = range.getBoundingClientRect();
-      const editorRect = editorRef.current.getBoundingClientRect();
-      const lineHeight = editorRef.current.offsetHeight / lineNumbers.length;
-      const position = Math.floor((rect.top - editorRect.top) / lineHeight);
-      setHighlightedLine(position);
-    });
+  const updateHighlightedLine = () => {
+    const selection = window.getSelection();    
+    setHighlightedLine(selection.focusOffset);
+  };
 
+  useEffect(() => {
+    editorRef.current.addEventListener('keyup', updateHighlightedLine);
+    editorRef.current.addEventListener('click', updateHighlightedLine);
     editorRef.current.addEventListener('scroll', (e) => {
       lineNumberRef.current.scrollTop = e.target.scrollTop;
     });
@@ -47,11 +44,13 @@ const TextEditor = () => {
     setLineNumbers(lines.map((_, index) => index + 1));
   };
 
+  const lineHeightStyle = '20px'; // Establece la altura de línea que desees
+
   return (
     <div style={{ display: 'flex', background: '#2c2c2c', height: '100vh', width: '50vw', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', overflow: 'hidden' }}>
-      <div ref={lineNumberRef} style={{ width: '50px', lineHeight: '1.2em', background: '#1a1a1a', padding: '10px 5px', color: '#b5b5b5', borderRight: '1px solid #aaa', overflowY: 'hidden' }}>
+      <div ref={lineNumberRef} style={{ width: '50px', lineHeight: lineHeightStyle, background: '#1a1a1a', padding: '10px 5px', color: '#b5b5b5', borderRight: '1px solid #aaa', overflowY: 'hidden' }}>
         {lineNumbers.map((line, index) => (
-          <div key={line} style={{ height: '1.2em', background: highlightedLine === index ? '#3a3a3a' : 'transparent' }}>
+          <div key={line} style={{ height: lineHeightStyle, background: highlightedLine === index ? '#3a3a3a' : 'transparent' }}>
             {line}
           </div>
         ))}
@@ -66,7 +65,7 @@ const TextEditor = () => {
           overflowY: 'auto',
           flex: 1,
           overflowWrap: 'break-word',
-          lineHeight: '1.2em',
+          lineHeight: lineHeightStyle,
           whiteSpace: 'pre-wrap',
           color: '#e5e5e5',
           fontSize: '16px',
