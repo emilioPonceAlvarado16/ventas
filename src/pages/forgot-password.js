70942943a6032cc67e47ae04ff57be9a2fe7e38a
Isaias Ponce <isaiasponce-12@hotmail.es>
2025-04-30 16:57:10 -0500

    simon

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ForgotPassword from "@/components/ForgotPassword";
-import { useAuth } from '@/hooks/useAuth';
-import Footer from '@/components/Footer';
+// import React from 'react';
+// import ForgotPassword from "@/components/ForgotPassword";
+// import { useAuth } from '@/hooks/useAuth';
+// import Footer from '@/components/Footer';
 
-export default function ForgotPasswordPage() {
-  const { forgotPassword, forgotPasswordError } = useAuth();
+// export default function ForgotPasswordPage() {
+//   const { forgotPassword, forgotPasswordError } = useAuth();
 
-  return (
-    <>
-    <ForgotPassword forgotPassword={forgotPassword} forgotPasswordError={forgotPasswordError} />
-    <Footer/>
-    </>
-  );
-}
+//   return (
+//     <>
+//     <ForgotPassword forgotPassword={forgotPassword} forgotPasswordError={forgotPasswordError} />
+//     <Footer/>
+//     </>
+//   );
+// }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '@/hooks/useAuth';
-import Login from '@/components/Login';
-import { useRouter } from 'next/router';
-import Loading from '@/components/Loading';
-export default function LoginPage() {
-  const { isAuthenticated, isLoading } = useAuth();
-  const router = useRouter();
+// import React, { useEffect } from 'react';
+// import { useAuth } from '@/hooks/useAuth';
+// import Login from '@/components/Login';
+// import { useRouter } from 'next/router';
+// import Loading from '@/components/Loading';
+// export default function LoginPage() {
+//   const { isAuthenticated, isLoading } = useAuth();
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (!isLoading && isAuthenticated) {
-      router.push('/home').then(() => {});
-    }
-  }, [isLoading, isAuthenticated, router]);
+//   useEffect(() => {
+//     if (!isLoading && isAuthenticated) {
+//       router.push('/home').then(() => {});
+//     }
+//   }, [isLoading, isAuthenticated, router]);
 
-  if (isLoading) {
-    return <Loading/>; 
-  }
+//   if (isLoading) {
+//     return <Loading/>; 
+//   }
 
-  return (
+//   return (
 
-    <Login/>
+//     <Login/>
   
-  )
-}
+//   )
+// }
